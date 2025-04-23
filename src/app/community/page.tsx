025a0e89bea873a34da01a9173220185c6b15184
Ritesh <riteshkd2k20@gmail.com>
2025-04-23 09:15:40 +0530

    community soon

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const index = () => {
-  return <div>community</div>;
+  return (
+    <div className="w-[98vw] h-screen justify-center flex items-center text-4xl font-bold text-slate-200">
+      Coming Soon
+    </div>
+  );
 };
 
 export default index;
